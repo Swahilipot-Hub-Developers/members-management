74debe1089b2f510dc451b37e16655087dc2e759
Chris Mwalimo <82896090+codeschris@users.noreply.github.com>
2023-11-01 10:11:10 +0300

    Configuring bootstrap and SASS

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <main>
+        <p className="text-end">Welcome to this site</p>
+        <button>Hello World</button>
+      </main>
+    </div>
+  )
+}
