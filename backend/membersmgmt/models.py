c4099af6905f1d8e73e7ad05ccf88d8efd5cc976
Ngugi <106905843+Joyngugs@users.noreply.github.com>
2023-11-03 10:04:21 +0000

    ddefining models for the database tables

---
+++ backend/membersmgmt/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class members(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other'),
+    )
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    year_of_birth = models.PositiveIntegerField()
+    phone_number = models.CharField(max_length=15)  # Assuming international format
+    country = models.CharField(max_length=100)
+    county = models.CharField(max_length=100)
+    sub_county = models.CharField(max_length=100)
