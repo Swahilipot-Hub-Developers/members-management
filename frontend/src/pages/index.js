e0b7ea730527ad5f1a90777239bc1c0446398c7b
Chris Mwalimo <82896090+codeschris@users.noreply.github.com>
2023-11-05 15:26:28 +0300

    theme changeanges

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,8 +1,8 @@
 const Home = () => {
   return (
     <>
-      <div className="container mt-4">
-        <h1 className="text-center">System is a work in progress!</h1>
+      <div className="container mt-5">
+        <h1 className="display-5 fw-bold text-center">System is a work in progress!</h1>
       </div>
     </>
   )
