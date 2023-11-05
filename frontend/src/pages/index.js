df78972271c27050c63b21ffec00b8cbeb8e3c36
Chris Mwalimo <82896090+codeschris@users.noreply.github.com>
2023-11-05 13:18:20 +0300

    Placeholder for main stage

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -2,7 +2,7 @@ const Home = () => {
   return (
     <>
       <div className="container mt-4">
-        <h1 className="text-center">Website is a work in progress!</h1>
+        <h1 className="text-center">System is a work in progress!</h1>
       </div>
     </>
   )
