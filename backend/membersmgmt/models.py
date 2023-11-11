a946bdff443bd83dfb7a42c40b6b50bb86c1b738
Ngugi <106905843+Joyngugs@users.noreply.github.com>
2023-11-11 09:13:02 +0000

    backend development

--- backend/membersmgmt/models.py
+++ backend/membersmgmt/models.py
@@ -10,7 +10,21 @@ class members(models.Model):
     )
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
     year_of_birth = models.PositiveIntegerField()
-    phone_number = models.CharField(max_length=15)  # Assuming international format
+    phone_number = models.CharField(max_length=15)
     country = models.CharField(max_length=100)
     county = models.CharField(max_length=100)
     sub_county = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class admin(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+    email_address = models.CharField(max_length=255)
+
+    def __str__(self):
+        return str(self.id)
