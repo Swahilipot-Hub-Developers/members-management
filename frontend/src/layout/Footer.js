ac988f052b7be632cc28e4e0682515361a7cd904
codeschris <chrismlolwa42@outlook.com>
2023-11-12 07:03:28 +0300

    Footer changes

--- frontend/src/layout/Footer.js
+++ frontend/src/layout/Footer.js
@@ -1,8 +1,37 @@
 const Footer = () => {
     const year = new Date().getFullYear();
     return (
-        <footer className="text-center bg-white mt-3 w-100 py-3">
-            <p className="text-muted m-0 px-5">&copy; Swahilipot Hub Foundation. {year}</p>
+        <footer class="container py-4">
+            <div class="row align-items-md-center text-center text-md-start">
+                <div class="col-md mb-3 mb-md-0">
+                    <p class="mb-0">© Swahilipot Hub Foundation {year}</p>
+                </div>
+
+                <div class="col-md d-md-flex justify-content-md-end">
+                    <ul class="list-inline mb-0">
+                        <li class="list-inline-item">
+                            <a class="btn btn-icon btn-sm btn-ghost-secondary rounded-circle" href="#">
+                                <i class="bi-facebook"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a class="btn btn-icon btn-sm btn-ghost-secondary rounded-circle" href="#">
+                                <i class="bi-twitter"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a class="btn btn-icon btn-sm btn-ghost-secondary rounded-circle" href="#">
+                                <i class="bi-github"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a class="btn btn-icon btn-sm btn-ghost-secondary rounded-circle" href="#">
+                                <i class="bi-slack"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </footer>
     );
 }
