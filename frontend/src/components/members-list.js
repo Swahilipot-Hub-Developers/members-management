f2c138b7d9497e0e2b9c3b361a8a18647683dec6
Ngugi <106905843+Joyngugs@users.noreply.github.com>
2023-11-15 19:46:26 +0000

    Frontend changes - Testing rendering of Members List
    Co-authored-by: Chris Mwalimo <chrismlolwa42@outlook.com>

--- frontend/src/components/members-list.js
+++ frontend/src/components/members-list.js
@@ -1,22 +1,35 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 const MembersList = () => {
+    const [members, setMembers] = useState([]);
+
+    useEffect(() => {
+        // Fetch members from the API
+        const fetchMembers = async () => {
+            try {
+                const response = await axios.get('http://127.0.0.1:8000/membersmgmt/members');
+                setMembers(response.data);
+            } catch (error) {
+                console.error('Error fetching members:', error);
+            }
+        };
+
+        // Call the fetchMembers function
+        fetchMembers();
+    }, []); // The empty dependency array ensures the effect runs only once
+
     return (
         <div>
             <ul className="list-group list-group-flush">
-                <li className="list-group-item">
-                    Christopher Mwalimo Mlolwa
-                </li>
-                <li className="list-group-item">
-                    Joy Ngugi Mburugu
-                </li>
-                <li className="list-group-item">
-                    Abdallah Khalfan
-                </li>
-                <li className="list-group-item">
-                    Daniel Kimanzi
-                </li>
+                {members.map(member => (
+                    <li key={member.id} className="list-group-item">
+                        {member.name}
+                    </li>
+                ))}
             </ul>
         </div>
-    )
+    );
 }
 
 export default MembersList;
