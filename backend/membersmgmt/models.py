4a5251372f4edaab3841c84b1cd81bf51650ce84
Chris Mwalimo <82896090+codeschris@users.noreply.github.com>
2023-11-16 12:55:48 +0300

    Backend changes - New migrations to the database - CORS functionality has been fixed
    Co-authored-by: Joy Ngugi <joyngugs20@gmail.com>

--- backend/membersmgmt/models.py
+++ backend/membersmgmt/models.py
@@ -1,15 +1,30 @@
 from django.db import models
 
 class Member(models.Model):
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other')
+    ]
+
+    SUB_COUNTY_CHOICES = [
+        ('Mvita', 'Mvita'),
+        ('Jomvu', 'Jomvu'),
+        ('Changamwe', 'Changamwe'),
+        ('Kisauni', 'Kisauni'),
+        ('Nyali', 'Nyali'),
+        ('Likoni', 'Likoni')
+    ]
+
     member_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    gender = models.CharField(max_length=10)
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
     year_of_birth = models.IntegerField()
     phone_number = models.CharField(max_length=15)
     email_address = models.EmailField()
     country = models.CharField(max_length=50, default='Kenya')
     county = models.CharField(max_length=50)
-    sub_county = models.CharField(max_length=50)
+    sub_county = models.CharField(max_length=50, choices=SUB_COUNTY_CHOICES)
 
     def __str__(self):
         return self.name
