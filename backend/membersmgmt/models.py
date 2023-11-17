f78f6b0f5254df0a672c6e39ad4be43f950259d1
Chris Mwalimo <82896090+codeschris@users.noreply.github.com>
2023-11-18 01:25:56 +0300

    Changes to the backend - member_id is custom 'SPH-Mxxx' - Delete function for members has been implemented - migrations, database and tables have been refreshed - models has been modified for the support of custom ids

--- backend/membersmgmt/models.py
+++ backend/membersmgmt/models.py
@@ -16,7 +16,7 @@ class Member(models.Model):
         ('Likoni', 'Likoni')
     ]
 
-    member_id = models.AutoField(primary_key=True)
+    member_id = models.CharField(max_length=20, primary_key=True, unique=True, default='SPH-M0000')
     name = models.CharField(max_length=100)
     gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
     year_of_birth = models.IntegerField()
@@ -26,8 +26,17 @@ class Member(models.Model):
     county = models.CharField(max_length=50)
     sub_county = models.CharField(max_length=50, choices=SUB_COUNTY_CHOICES)
 
-    def __str__(self):
-        return self.name
+    def save(self, *args, **kwargs):
+        if not self.member_id or self.member_id == 'SPH-M0000':
+            last_member = Member.objects.order_by('-member_id').first()
+            if last_member:
+                last_id = int(last_member.member_id.split('-M')[-1])
+            else:
+                last_id = 0
+            new_id = 'SPH-M{:04d}'.format(last_id + 1)
+            self.member_id = new_id
+
+        super().save(*args, **kwargs)
 
 class Admin(models.Model):
     id = models.AutoField(primary_key=True)
