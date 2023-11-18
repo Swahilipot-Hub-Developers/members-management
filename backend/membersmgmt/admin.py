ff30a8cae4bf399d03b7ad700065c7fa43247a90
Chris Mwalimo <82896090+codeschris@users.noreply.github.com>
2023-11-18 13:57:30 +0300

    typo change

--- backend/membersmgmt/admin.py
+++ backend/membersmgmt/admin.py
@@ -4,5 +4,5 @@ from .models import Member
 @admin.register(Member)
 class MemberAdmin(admin.ModelAdmin):
     list_display = ('member_id', 'name', 'gender', 'year_of_birth', 'phone_number', 'email_address', 'country', 'county', 'sub_county')
-    search_fields = ('member_id', 'name', 'gender', 'year_of_birth', 'phone_number', 'email_address', 'country', 'county', 'sub_county')  # Add any other fields you want to be searchable
+    search_fields = ('member_id', 'name', 'gender', 'year_of_birth', 'phone_number', 'email_address', 'country', 'county', 'sub_county')
 
