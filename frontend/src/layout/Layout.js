619437135d9adaf088ccdc583440cbfa004eab74
Chris Mwalimo <82896090+codeschris@users.noreply.github.com>
2023-11-20 15:10:45 +0300

    hiding nav bar

--- frontend/src/layout/Layout.js
+++ frontend/src/layout/Layout.js
@@ -1,12 +1,16 @@
 import NavBar from './NavBar';
 import Footer from './Footer';
 import MetaTags from './Metatags';
+import { useRouter } from 'next/router';
 
 const Layout = ({ children }) => {
+  const router = useRouter();
+  const hideNavbar = router.pathname === '/sessions/register-member'; 
+
   return (
     <>
       <MetaTags/>
-      <NavBar />
+      {!hideNavbar && <NavBar />}
       <main className='container'>{children}</main>
       <Footer />
     </>
