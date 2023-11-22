cbf8a8b6b7c471b0181340034a98be1c5cf126b3
Chris Mwalimo <82896090+codeschris@users.noreply.github.com>
2023-11-22 20:52:42 +0300

    workflow test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = "django-insecure--fmwckg#6yp*9n_2-%l-!t5g9r(%@1n4r0f*bzd%0a(ucmxnn^" # config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
